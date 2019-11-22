6113fb322eb309cae94ec9f127eeb3b781ec678e
honorable-exception-1170 <aliciachao@gmail.com>
2019-11-22 23:51:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,13 @@
 def square_array(array)
   # your code here
+  squared_array = []
   counter = 0
+
   while counter < array.length do
     squared_number = array[counter] **= 2
-    squared_array = []
     squared_array.push(squared_number)
     counter += 1
   end
+
 squared_array
 end
