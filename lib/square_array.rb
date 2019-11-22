df0949cb726318814f104011456fe8fdbfded96c
honorable-exception-1170 <aliciachao@gmail.com>
2019-11-22 23:34:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,9 @@ def square_array(array)
   # your code here
   counter = 0
   while counter < array.length
-    array[counter] **= 2
-    
+    puts array[counter] **= 2
+
 end
+
+# numbers = [1,2,3]
+# square_array(numbers)
